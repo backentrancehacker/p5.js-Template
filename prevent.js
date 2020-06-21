f3c522113f80227bbef8279a6280cf115615de5a
NathanFS <backentrancehacker@gmail.com>
2020-06-21 06:38:30 +0000

    upgraded prevent.js

--- prevent.js
+++ prevent.js
@@ -1,10 +1,6 @@
-document.addEventListener('contextmenu', (e) => {
-  	e.preventDefault()
-	return false
-})
+document.addEventListener('contextmenu', (e) => (e.preventDefault()))
+
 document.addEventListener('keydown', (e) => {
-	if((e.keyCode == 123 ) || (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) || (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) || (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) || (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0))){
-		e.preventDefault()
-		return false
-	}
+	if((e.keyCode == 123 ) || (e.ctrlKey && (e.keyCode == 'U'.charCodeAt(0) || (e.shiftKey && (e.keyCode == 'U'.charCodeAt(0) || e.keyCode == 'C'.charCodeAt(0) ||  e.keyCode == 'J'.charCodeAt(0) || e.keyCode == 'I'.charCodeAt(0)))))) 
+		e.preventDefault()	
 })
